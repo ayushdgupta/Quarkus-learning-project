4588a5e520e977b52f317c0f4e03c2ac494a8bc9
Ayush Gupta <ayushdgupta17@gmail.com>
2023-03-13 10:02:25 +0530

    add two more endpoints 'PUT' and 'Delete' in our Mobile API

--- src/main/java/com/guptaji/resources/MobileResource.java
+++ src/main/java/com/guptaji/resources/MobileResource.java
@@ -25,4 +25,18 @@ public class MobileResource {
     public Response addNewMobileName(String mobileName){
         return Response.ok(MobileUtility.addNewMobileName(mobileName)).build();
     }
+
+    @PUT
+    @Path("/{oldMobileName}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateMobileName(@PathParam("oldMobileName") String oldName, @QueryParam("newMobileName") String newName){
+        return Response.ok(MobileUtility.updateMobileName(oldName, newName)).build();
+    }
+
+    @DELETE
+    @Path("/{mobileName}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteMobileNumber(@PathParam("mobileName") String mobileNameToBeDeleted){
+        return MobileUtility.deleteMobileNumber(mobileNameToBeDeleted);
+    }
 }
--- src/main/java/com/guptaji/util/MobileUtility.java
+++ src/main/java/com/guptaji/util/MobileUtility.java
@@ -1,7 +1,9 @@
 package com.guptaji.util;
 
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MobileUtility {
 
@@ -22,5 +24,27 @@ public class MobileUtility {
             mobileList.add(mobileName);
         }
         return mobileList;
+    }
+
+    public static List<String> updateMobileName(String oldName, String newName){
+        List<String> resultantList = mobileList.stream().map(tempMobileName -> {
+            if (tempMobileName.equalsIgnoreCase(oldName)) {
+                return newName;
+            } else {
+                return tempMobileName;
+            }
+        }).collect(Collectors.toList());
+        return resultantList;
+    }
+
+    public static Response deleteMobileNumber(String mobileName){
+        if (mobileList.contains(mobileName)){
+            mobileList = mobileList.stream().filter(tempMobileName -> !tempMobileName.equalsIgnoreCase(mobileName)).collect(Collectors.toList());
+            return Response.ok(mobileList).build();
+        }
+        // we can also use simple remove function
+        // resultantList.remove(mobileName);
+        // return Response.noContent().build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
